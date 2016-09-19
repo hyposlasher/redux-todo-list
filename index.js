c0af7ca6ae1a7b68a66665374eef303f901a51ce
Маушов Динислам Каримуллаевич <dkmaushov@sbn-mac-24.local>
2016-09-19 19:50:37 +0300

    123

--- index.js
+++ index.js
@@ -1,60 +1,15 @@
-import React from 'react';
-import { createStore } from 'redux';
-import { Provider, connect } from 'react-redux';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import App from './components/App'
 
-// React component
-class Counter extends React.Component {
-  render(){
-    const { value, onIncreaseClick } = this.props;
-    return (
-      <div>
-        <span>{value}</span>
-        <button onClick={onIncreaseClick}>Increase</button>
-      </div>
-    );
-  }
-}
+let store = createStore(todoApp)
 
-// Action:
-const increaseAction = {type: 'increase'};
-
-// Reducer:
-function counter(state={count: 0}, action) {
-  let count = state.count;
-  switch(action.type){
-    case 'increase':
-      return {count: count+1};
-    default:
-      return state;
-  }
-}
-
-// Store:
-let store = createStore(counter);
-
-// Map Redux state to component props
-function mapStateToProps(state)  {
-  return {
-    value: state.count
-  };
-}
-
-// Map Redux actions to component props
-function mapDispatchToProps(dispatch) {
-  return {
-    onIncreaseClick: () => dispatch(increaseAction)
-  };
-}
-
-// Connected Component:
-let App = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Counter);
-
-React.render(
+render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
-);
+)
